8f121b51f3a60611d0272fe2760b82de62544eec
Karl Stolley <karl.stolley@gmail.com>
2018-01-24 16:08:33 -0600

    Grab and show submitted email address

--- site.js
+++ site.js
@@ -13,7 +13,8 @@ $('#show').on('click',
 $('#contact-form').on('submit',
   function() {
     console.log('The form was submitted.');
+    var email = $('#email').val();
     $(this).remove();
-    $('body').append('<h1>Thank You for Your Email Address</h1>');
+    $('body').append('<h1>Thank You. Email Address: ' + email + '</h1>');
   }
 );
