fa9b1513a9194e63a470178ea3508c2b16d551cf
Karl Stolley <karl.stolley@gmail.com>
2018-02-28 16:02:33 -0600

    Add .fx class after 500ms delay

--- site.js
+++ site.js
@@ -1,6 +1,12 @@
 // Clicking anywhere in the li focuses the child <input>
 $('html').removeClass('nojs').addClass('js');
 
+var loadFX = function() {
+  $('html').addClass('fx');
+}
+
+setTimeout(loadFX, 500);
+
 $('.inputs li').on('click', function() {
   $(this).find('input').focus();
 });
