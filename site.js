877bdf2988691b3eb2dcf367b26b727070da44f8
Karl Stolley <karl.stolley@gmail.com>
2018-03-07 16:08:20 -0600

    Add basic JavaScript steppers to servings input

--- site.js
+++ site.js
@@ -10,3 +10,24 @@ setTimeout(loadFX, 500);
 $('.inputs li').on('click', function() {
   $(this).find('input').focus();
 });
+
+/* Create steppers for servings */
+/* Add stepper elements */
+/* TODO: Make sure these controls are accessible */
+$('#servings').before('<a id="more" href="#null">+</a>');
+$('#servings').after('<a id="less" href="#null">-</a>');
+
+$('#more').on('click', function(e) {
+  var current_value = $('#servings').val();
+  var new_value = parseInt(current_value, 10) + 1;
+  $('#servings').val(new_value);
+});
+
+$('#less').on('click', function(e) {
+  var current_value = $('#servings').val();
+  var new_value = parseInt(current_value, 10) - 1;
+  if (new_value < 0) {
+    new_value = 0;
+  }
+  $('#servings').val(new_value);
+});
