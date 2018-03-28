14817b6afe08b4ce3be9e4dc5728e2bd2abce035
Karl Stolley <karl.stolley@gmail.com>
2018-03-28 12:46:13 -0500

    Add starter files for seating demo

---
+++ seating/site.js
@@ -0,0 +1,12 @@
+$('html').removeClass('nojs').addClass('js');
+
+// Add the .fx class after a short delay to avoid triggering any animations on DOM loads
+var loadFX = function() {
+  $('html').addClass('fx');
+}
+setTimeout(loadFX, 500);
+
+// Focus inner inputs when li is clicked/tapped
+$('.inputs li').on('click', function() {
+  $(this).find('input').focus();
+});
